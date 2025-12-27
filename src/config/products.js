2ae0d154e75469ae84dd5b8889275b7f5f0223b2
Coty Janeway <coty.janeway@gmail.com>
2025-12-27 13:30:38 -0700

    reshape products to just have urls

---
+++ src/config/products.js
@@ -0,0 +1,7 @@
+// URLS. Defined manually
+const PRODUCTS = {
+  toothpaste: "https://www.amazon.com/Crest-Complete-Whitening-Toothpaste-Triple/dp/B005PLQIQ4?_encoding=UTF8&fpw=new&fpl=fresh&pd_rd_w=pPi2W&content-id=amzn1.sym.68e2229f-94c7-48df-aa84-90b872f33a3b&pf_rd_p=68e2229f-94c7-48df-aa84-90b872f33a3b&pf_rd_r=3GBWJRWMQHQQSG4H44WE&pd_rd_wg=1yzJQ&pd_rd_r=55bb53ff-23b3-4906-8d28-96a9f35f056a&pd_rd_i=B005PLQIQ4",
+  monitor:    "https://www.amazon.com/SAMSUNG-inch-UJ59-monitor-LU32J590UQNXZA/dp/B07CS3JGPC?crid=ORN3635HJ2KZ&dib=eyJ2IjoiMSJ9.rZ40OD_qDbzHBkZoeTkfIqZJbP403KV9wRQiuDxRnNz_ZRSm-6-YsprLGfsU_92yPcgUcwC8B14cT19_1bkVvvNU_WO_yBhIBYa07x113XSf3MA8GfcjguepZvrI5zcIWVFxweMWP3UnMI72N1RTEklW03Mw3aOh_RWACw0PDNeS0wU4GG738whZQulEOt3L9PDLvcmQnvUfeWEaG8Ywvvf7GFniNQga7XtcWZ5qyO4.GlIQTRu7SjW1xxuAyUj5P_dxUEPe9QTKilS5oIMygRs&dib_tag=se&keywords=4k%2Bmonitor&qid=1766440877&refinements=p_n_g-101013577303111%3A72511692011&rnid=72511690011&sprefix=4k%2Bmoni%2Caps%2C202&sr=8-7&th=1",
+}
+
+export default PRODUCTS;
