774b58079806b55ccd4289fe5298ce671d7fc0d1
Coty Janeway <coty.janeway@gmail.com>
2026-01-05 13:39:52 -0700

    update to deploy from source

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
-
-  base: '/', // browser URL prefix
+  base: '/',
   build: {
-    outDir: '../CotyJ.github.io', // target Pages repo folder
+    outDir: 'dist',
     emptyOutDir: true,
   },
 });
