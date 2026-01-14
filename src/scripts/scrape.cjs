633c1992c67be7bfad04f7b1fba789e6609337a5
Coty Janeway <coty.janeway@gmail.com>
2026-01-13 19:34:42 -0700

    fix data collection and saving, hopefully actions is happy

--- src/scripts/scrape.cjs
+++ src/scripts/scrape.cjs
@@ -9,39 +9,42 @@ const PRODUCTS = require(productPath);
 const DATA = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
 // Get title, imageURL, and price for an item
-const getItemInfo = async (entry) => {
+const getItemInfo = async (entry, webPage) => {
   const entryUrl = entry[1];
 
+  try {
+    await webPage.goto(entryUrl, { waitUntil: 'networkidle2', timeout: 60000 });
+    await webPage.waitForSelector('#productTitle',{timeout: 60000});
+    const title = await webPage.$eval('#productTitle', (el) => el.textContent.trim());
+    const priceDollar = await webPage.$eval('.a-price-whole', (el) => el.textContent.trim());
+    const priceCents = await webPage.$eval('.a-price-fraction', (el) => el.textContent.trim());
+    const price = priceDollar.concat(priceCents);
+    const imageURL = await webPage.$eval('#landingImage', (el) => el.src);
+
+    return [title, imageURL, price];
+
+  } catch (err) {
+    console.log("Get network failed! \n", err);
+  }
+};
+
+const scraper = async (products) => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
   });
   const page = await browser.newPage();
-
-  try {
-    await page.goto(entryUrl, { waitUntil: 'networkidle2', timeout: 60000 });
-    await page.waitForSelector('#productTitle',{timeout: 60000});
-    const title = await page.$eval('#productTitle', (el) => el.textContent.trim());
-    const priceDollar = await page.$eval('.a-price-whole', (el) => el.textContent.trim());
-    const priceCents = await page.$eval('.a-price-fraction', (el) => el.textContent.trim());
-    const price = priceDollar.concat(priceCents);
-    const imageURL = await page.$eval('#landingImage', (el) => el.src);
-
-    return [title, imageURL, price];
-  } catch (err) {
-    console.log(err);
-  } finally {
-    await browser.close();
-  }
-};
-
-const scraper = async (products) => {
   const newDATA = {};
+
   try {
-    for (const item of Object.entries(products)) {
-      if (item[0] === 'default') {
-        for (const entry of Object.entries(item[1])) {
-          const result = await getItemInfo(entry);
+
+    for (const [rType, item] of Object.entries(products)) {
+
+      if (rType === 'default') {
+
+        for (const entry of Object.entries(item)) {
+          const result = await getItemInfo(entry, page);
+
           if (!result) {
             console.warn(`Skipping ${entry[0]} — scrape failed`);
             continue;
@@ -50,7 +53,7 @@ const scraper = async (products) => {
           const newName = entry[0];
           const today = new Date().toISOString();
           const latest = today.slice(0, 10);
-          const newest = DATA[newName]?.history[0]?.date?.slice(0, 10);
+          const newest = DATA[newName]?.history[DATA[newName].history.length - 1]?.date?.slice(0, 10);
           const prevHistory = DATA[newName]?.history || [];
 
           if (newest !== latest) {
@@ -58,27 +61,42 @@ const scraper = async (products) => {
               title,
               url,
               history: [
+                ...prevHistory,
                 {
                   date: latest,
                   price: price,
                 },
-                ...prevHistory,
               ],
             };
-
-            fs.writeFileSync(
-              dataPath,
-              JSON.stringify({ ...DATA, ...newDATA }, null, 2),
-              'utf-8'
-            );
           } else {
             console.log('Skip because I already ran today!');
           }
         }
       }
     }
-  } catch (error) {
-    console.log(error);
+
+    // Copy new data
+    const mergedData = { ...DATA };
+
+    for (const [key, value] of Object.entries(newDATA)) {
+      mergedData[key] = {
+        ...DATA[key] || {},
+        ...value
+      }
+    }
+
+
+  // Write out
+  fs.writeFileSync(
+    dataPath,
+    JSON.stringify(mergedData, null, 2),
+    'utf-8'
+  );
+  } catch (err) {
+      console.log(err);
+  } finally {
+
+      await browser.close();
   }
 };
 
